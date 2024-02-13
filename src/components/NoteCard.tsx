25c70592405aa531c9c8f809f5d49679661abc0f
Bianca Espíndola <32000570+biancaespindola@users.noreply.github.com>
2024-02-13 12:55:53 +0100

    Addind focus visible to the Note Cards Element

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,12 +1,12 @@
 export function NoteCard() {
   return (
-    <div className="rounded-md p-5 bg-slate-800 space-y-3 overflow-hidden relative">
+    <button className="rounded-md flex flex-col gap-3 text-left bg-slate-700 p-5 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
       <span className="text-sm font-medium text-slate-300">6 days ago</span>
       <p className="text-sm leading-6 text-slate-400">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
         labore et aut facilis.
       </p>
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none "></div>
-    </div>
+    </button>
   );
 }
